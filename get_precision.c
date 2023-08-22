d828a33771e5e242f4bfcef5b1601d49676ba120
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-22 04:14:30 -0700

    Printf Group Project

--- get_precision.c
+++ get_precision.c
@@ -3,8 +3,8 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current position in the format string
- * @list: List of arguments
+ * @i: List of arguments to be printed.
+ * @list: List of arguments.
  *
  * Return: Precision.
  */
@@ -13,14 +13,29 @@ int get_precision(const char *format, int *i, va_list list)
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	UNUSED(list);
+	if (format[curr_i] != '.')
+		return (precision);
 
-	if (format[curr_i] == 'r')
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		precision = curr_i - (*i + 1);
-		*i = curr_i;
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
+
+	*i = curr_i - 1;
 
 	return (precision);
 }
-
