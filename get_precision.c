28725b935d932bf4639d7ac8cfd1476755c1182c
KipruttoMutai <mutaibrian04@gmail.com>
2023-08-22 02:07:01 -0700

    Printf Group Project

--- get_precision.c
+++ get_precision.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
@@ -37,3 +39,4 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+
